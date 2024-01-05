7324174c87d7c64e7526ea3a958cc08f60eb8639
arieh.farber <arieh.farber@emil.com>
2024-01-05 18:16:49 +0200

    "update calculator"

--- ds/src/calculator.c
+++ ds/src/calculator.c
@@ -6,6 +6,7 @@
 #include <stdlib.h> /*strtod*/
 #include <string.h> /*strlen*/
 #include <assert.h> /*assert*/
+#include <math.h>   /*pow   */
 
 #include "calculator.h"
 #include "stack.h"
@@ -31,6 +32,7 @@ typedef enum
     MINUS,
     MULTIPLY,
     DIVIDE,
+	EXPONENT,
     SPACE,
     TAB,
     END,
@@ -38,7 +40,7 @@ typedef enum
 }events_t;
 
 static int status = SUCCESS;
-static int e_state = WAITING_FOR_OPERAND;
+static int state = WAITING_FOR_OPERAND;
 
 typedef double operator_handler_t(double, double);
 typedef char *expression_handler_t(char *, stack_t *, stack_t *);
@@ -51,20 +53,25 @@ static char *SyntaxError(char *str, stack_t *num_stack, stack_t *oper_stack);
 static char *PushOperand(char *str, stack_t *num_stack, stack_t *oper_stack);	 
 static char *OpenBracket(char *str, stack_t *num_stack, stack_t *oper_stack);
 static char *PushOperator(char *str, stack_t *num_stack, stack_t *oper_stack);
+static char *CheckOperator(char *str, stack_t *num_stack, stack_t *oper_stack);
 static char *CloseBrackets(char *str, stack_t *num_stack, stack_t *oper_stack);
 static char *SkipSpaces(char *str, stack_t *num_stack, stack_t *oper_stack);
-static char *FinalCalculate(char *str, stack_t *num_stack, stack_t *oper_stack);
-						 
+static char *FinalOperation(char *str, stack_t *num_stack, stack_t *oper_stack);
+
+static void ApplyOperation(stack_t *num_stack, stack_t *oper_stack);
+static status_t CheckMathErrors(char operator, double value1, double value2);
+
+static status_t InitializeAll(size_t size, stack_t **num_stack, \
+							  stack_t **oper_stack);					 
 static void InitlizeEventLUT();
 static void InitlizeOperatorLUT();
 static void InitlizeMatrixOfFunctions();
-
-static void ApplyOperation(stack_t *num_stack, stack_t *oper_stack);
 
 static double Addition(double value1, double value2);
 static double Subtraction(double value1, double value2);
 static double Divide(double value1, double value2);
 static double Multiply(double value1, double value2);
+static double Pow(double value1, double value2);
 
 status_t Calculate(const char *expression, double *res)
 {
@@ -74,31 +81,14 @@ status_t Calculate(const char *expression, double *res)
 	stack_t *oper_stack = NULL;
 	
     assert(NULL != expression);
-    
-    runner = (char *)expression;
-     
-    num_stack = StackCreate(strlen(expression), sizeof(double));
-    if (NULL == num_stack)
-    {
-		return (ALLOCATION_ERROR);
-    }
-    oper_stack = StackCreate(strlen(expression), sizeof(char));
-    if (NULL == num_stack)
-    {
-		StackDestroy(num_stack);
-		
-		return (ALLOCATION_ERROR);
-    }
-    
-    InitlizeEventLUT();
-    InitlizeOperatorLUT();
-    InitlizeMatrixOfFunctions();
+   
+	runner = (char *)expression;
+	status = InitializeAll(strlen(expression), &num_stack, &oper_stack);
 
 	while (END != event && SUCCESS == status)
 	{
 		event = event_lut[(size_t)(*runner)];
-				 
-		runner = function_matrix[e_state][event](runner, num_stack, oper_stack); 
+		runner = function_matrix[state][event](runner, num_stack, oper_stack); 
 	}
 	    
    	*res = *(double *)StackPeek(num_stack);
@@ -111,12 +101,10 @@ status_t Calculate(const char *expression, double *res)
 
 static char *SyntaxError(char *str, stack_t *num_stack, stack_t *oper_stack)
 {
-	double error_result = 0;
+	(void)num_stack;
 	(void)oper_stack;
 	
 	status = SYNTAX_ERROR;
-	
-	StackPush(num_stack, &error_result);
 	
 	return ((char *)str);
 }
@@ -131,7 +119,7 @@ static char *PushOperand(char *str, stack_t *num_stack, stack_t *oper_stack)
 	
 	StackPush(num_stack, &data);
 			
-	e_state = WAITING_FOR_OPERATOR;
+	state = WAITING_FOR_OPERATOR;
 	
 	return (endptr);
 }
@@ -145,7 +133,7 @@ static char *OpenBracket(char *str, stack_t *num_stack, stack_t *oper_stack)
 	
 	++runner;
 	
-	e_state = WAITING_FOR_OPERAND;
+	state = WAITING_FOR_OPERAND;
 	
 	return (runner);
 }
@@ -155,21 +143,45 @@ static char *PushOperator(char *str, stack_t *num_stack, stack_t *oper_stack)
 	char top_operator = 0;
 	char *runner = (char *)str;
 
-	if (TRUE != StackIsEmpty(oper_stack))
+	top_operator = *(char *)StackPeek(oper_stack);
+	if (event_lut[(size_t)top_operator] >= event_lut[(size_t)*str])
 	{
-		top_operator = *(char *)StackPeek(oper_stack);
-		if (event_lut[(size_t)top_operator] >= event_lut[(size_t)*str] && \
-			SUCCESS == status)
-		{
-			ApplyOperation(num_stack, oper_stack);	
-		}
+		ApplyOperation(num_stack, oper_stack);	
 	}
-	
+
 	StackPush(oper_stack, (void *)str);
 	
 	++runner;
 	
-	e_state = WAITING_FOR_OPERAND;
+	state = WAITING_FOR_OPERAND;
+	
+	return (runner);
+}
+
+static char *CheckOperator(char *str, stack_t *num_stack, stack_t *oper_stack)
+{
+	char top_operator = 0;
+	char *runner = (char *)str;
+	static char plus = '+';
+	static char minus = '-';
+	(void)num_stack;
+	(void)oper_stack;
+
+	top_operator = *(char *)StackPeek(oper_stack);
+	if (minus == top_operator && minus == *str)
+	{
+		StackPop(oper_stack);
+		StackPush(oper_stack, &plus);
+	}
+	else if (plus == top_operator && minus == *str)
+	{
+		StackPop(oper_stack);
+		StackPush(oper_stack, &minus);
+	}
+	
+	++runner;
+	
+	state = WAITING_FOR_OPERAND;
 	
 	return (runner);
 }
@@ -180,15 +192,22 @@ static char *CloseBrackets(char *str, stack_t *num_stack, stack_t *oper_stack)
 	(void)num_stack;
 	(void)oper_stack;
 	
-	while ('(' != *(char *)StackPeek(oper_stack) && SUCCESS == status)
+	while (TRUE != StackIsEmpty(num_stack) && \
+		   '(' != *(char *)StackPeek(oper_stack))
 	{
-		ApplyOperation(num_stack, oper_stack);
+		while (SUCCESS == status)
+		{
+			ApplyOperation(num_stack, oper_stack);	
+		}
 	}
-	StackPop(oper_stack);
+	if (TRUE != StackIsEmpty(num_stack))
+	{
+		StackPop(oper_stack);
+	}
 	
 	++runner;
 	
-	e_state = WAITING_FOR_OPERATOR;
+	state = WAITING_FOR_OPERATOR;
 	
 	return (runner);
 }
@@ -207,7 +226,7 @@ static char *SkipSpaces(char *str, stack_t *num_stack, stack_t *oper_stack)
 	return (runner);
 }
 
-static char *FinalCalculate(char *str, stack_t *num_stack, stack_t *oper_stack)
+static char *FinalOperation(char *str, stack_t *num_stack, stack_t *oper_stack)
 {
 	while (FINAL_RESULT != StackSize(num_stack) && SUCCESS == status)
 	{
@@ -222,27 +241,42 @@ static void ApplyOperation(stack_t *num_stack, stack_t *oper_stack)
 	double value1 = 0;
 	double value2 = 0;
 	double current_result = 0;
-	char current_operator = '\0';
+	char operator = '\0';
 
 	value1 = *(double *)StackPeek(num_stack);
 	StackPop(num_stack);
 
+	operator = *(char *)StackPeek(oper_stack);
+	StackPop(oper_stack);
+
 	value2 = *(double *)StackPeek(num_stack);
 	StackPop(num_stack);
-
-	current_operator = *(char *)StackPeek(oper_stack);
-	StackPop(oper_stack);
 	
-	if (0 == value1 && '/' == current_operator)
+	status = CheckMathErrors(operator, value1, value2);
+	if (SUCCESS != status)
 	{
-		status = MATH_ERROR;
 		return;
 	}
 
-	current_result = operator_lut[event_lut[(size_t)current_operator]](value1, value2);
+	current_result = operator_lut[event_lut[(size_t)operator]](value1, value2);
 
 	StackPush(num_stack, &current_result);
-}	
+}
+
+static status_t CheckMathErrors(char operator, double value1, double value2)
+{
+	if (0 == value1 && '/' == operator)
+	{
+		return (MATH_ERROR);
+	}
+
+	if (0 == value1 && 0 == value2 && '^' == operator)
+	{
+		return (MATH_ERROR);
+	}
+	
+	return (SUCCESS);
+}
 
 static double Addition(double value1, double value2)
 {
@@ -264,6 +298,43 @@ static double Multiply(double value1, double value2)
 	return (value2 * value1);
 }
 
+double Pow(double value1, double value2)
+{
+    return (pow(value2, value1));
+}
+
+static status_t InitializeAll(size_t size, stack_t **num_stack, \
+							  stack_t **oper_stack)
+{
+    static double defult_result = 0;
+	static char defult_operator = '+';
+
+	*num_stack = StackCreate(size, sizeof(double));
+    if (NULL == *num_stack)
+    {
+        return (ALLOCATION_ERROR);
+    }
+
+    *oper_stack = StackCreate(size, sizeof(char));
+    if (NULL == *oper_stack)
+    {
+        StackDestroy(*num_stack);
+
+       return (ALLOCATION_ERROR);
+    }
+
+    state = WAITING_FOR_OPERAND;
+
+    InitlizeEventLUT();
+    InitlizeOperatorLUT();
+    InitlizeMatrixOfFunctions();
+
+	StackPush(*num_stack, &defult_result);
+	StackPush(*oper_stack, &defult_operator);
+
+    return (SUCCESS);
+}
+
 static void InitlizeEventLUT()
 {
 	event_lut['0'] = NUM;
@@ -276,12 +347,13 @@ static void InitlizeEventLUT()
 	event_lut['7'] = NUM;
 	event_lut['8'] = NUM;
 	event_lut['9'] = NUM;
+	event_lut['('] = OPEN_ROUND_BRACKET;
+	event_lut[')'] = CLOSE_ROUND_BRACKET;
 	event_lut['+'] = PLUS;
 	event_lut['-'] = MINUS;
 	event_lut['*'] = MULTIPLY;
 	event_lut[ '/'] = DIVIDE;
-	event_lut['('] = OPEN_ROUND_BRACKET;
-	event_lut[')'] = CLOSE_ROUND_BRACKET;
+	event_lut['^'] = EXPONENT;
 	event_lut[' '] = SPACE;
 	event_lut['\t'] = TAB;
 	event_lut['\0'] = END;
@@ -293,31 +365,34 @@ static void InitlizeOperatorLUT()
 	operator_lut[MINUS] = Subtraction;
 	operator_lut[MULTIPLY] = Multiply;
 	operator_lut[DIVIDE] = Divide;
+	operator_lut[EXPONENT] = Pow;
 }
 
 static void InitlizeMatrixOfFunctions()
 {
     function_matrix[WAITING_FOR_OPERAND][OTHER] = SyntaxError;
     function_matrix[WAITING_FOR_OPERAND][NUM] = PushOperand;
-    function_matrix[WAITING_FOR_OPERAND][PLUS] = SyntaxError;
-    function_matrix[WAITING_FOR_OPERAND][MINUS] = SyntaxError;
+	function_matrix[WAITING_FOR_OPERAND][OPEN_ROUND_BRACKET] = OpenBracket;
+    function_matrix[WAITING_FOR_OPERAND][CLOSE_ROUND_BRACKET] = SyntaxError;
+    function_matrix[WAITING_FOR_OPERAND][PLUS] = CheckOperator;
+    function_matrix[WAITING_FOR_OPERAND][MINUS] = CheckOperator;
     function_matrix[WAITING_FOR_OPERAND][MULTIPLY] = SyntaxError;
     function_matrix[WAITING_FOR_OPERAND][DIVIDE] = SyntaxError;
-    function_matrix[WAITING_FOR_OPERAND][OPEN_ROUND_BRACKET] = OpenBracket;
-    function_matrix[WAITING_FOR_OPERAND][CLOSE_ROUND_BRACKET] = SyntaxError;
+	function_matrix[WAITING_FOR_OPERAND][EXPONENT] = SyntaxError;
     function_matrix[WAITING_FOR_OPERAND][SPACE] = SkipSpaces;
     function_matrix[WAITING_FOR_OPERAND][TAB] = SkipSpaces;
     function_matrix[WAITING_FOR_OPERAND][END] = SyntaxError;
     
     function_matrix[WAITING_FOR_OPERATOR][OTHER] = SyntaxError;
     function_matrix[WAITING_FOR_OPERATOR][NUM] = SyntaxError;
+    function_matrix[WAITING_FOR_OPERATOR][OPEN_ROUND_BRACKET] = OpenBracket;
+    function_matrix[WAITING_FOR_OPERATOR][CLOSE_ROUND_BRACKET] = CloseBrackets;
     function_matrix[WAITING_FOR_OPERATOR][PLUS] = PushOperator;
     function_matrix[WAITING_FOR_OPERATOR][MINUS] = PushOperator;
     function_matrix[WAITING_FOR_OPERATOR][MULTIPLY] = PushOperator;
     function_matrix[WAITING_FOR_OPERATOR][DIVIDE] = PushOperator;
-    function_matrix[WAITING_FOR_OPERATOR][OPEN_ROUND_BRACKET] = SyntaxError;
-    function_matrix[WAITING_FOR_OPERATOR][CLOSE_ROUND_BRACKET] = CloseBrackets;
+	function_matrix[WAITING_FOR_OPERATOR][EXPONENT] = PushOperator;
     function_matrix[WAITING_FOR_OPERATOR][SPACE] = SkipSpaces;
     function_matrix[WAITING_FOR_OPERATOR][TAB] = SkipSpaces;
-    function_matrix[WAITING_FOR_OPERATOR][END] = FinalCalculate;
+    function_matrix[WAITING_FOR_OPERATOR][END] = FinalOperation;
 }
--- ds/test/calculator_test.c
+++ ds/test/calculator_test.c
@@ -25,13 +25,12 @@ static void TestCalculate()
 	char *expressions[] = {"7+8", "8+83+-2^5", "8+83-2^", "2/0", \
 						   "8++8((3-2)5)", "3-2)5", "(3-2)5+ 5(4+4+4"};
 	double res = 0;
-	double want_res[] =   {15, 49, 0, 0, \
-						   48, 0, 0};
+	double want_res[] = {15, 49, 0, 0, 48, 0, 0};
 
-	size_t expressions_array_size = sizeof(expressions) / sizeof(expressions[0]);
+	size_t expressions_size = sizeof(expressions) / sizeof(expressions[0]);
 	size_t i = 0;
 	
-	for (i = 0; i < expressions_array_size; ++i)
+	for (i = 0; i < expressions_size; ++i)
 	{
 		got_status = Calculate(expressions[i], &res);
 		TestInt(want_status[i], got_status, __LINE__);
